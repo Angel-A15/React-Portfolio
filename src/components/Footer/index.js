c380a06b95e1b7922f8cb9c20a0cd88dfbe4393e
Angel-A15 <106582411+Angel-A15@users.noreply.github.com>
2022-11-29 01:41:32 -0600

    working on footer, and project componenets

---
+++ src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <section>
+            <p><a href="https://github.com/Angel-A15">GitHub icon</a></p>
+            <p><a href="https://www.linkedin.com/feed/">LinkedIn icon</a></p>
+            <p><a href="https://stackoverflow.com/users/19176451/angel-aguilar">stackoverflow icon</a></p>
+        </section>
+    );
+}
+
+export default Footer;
