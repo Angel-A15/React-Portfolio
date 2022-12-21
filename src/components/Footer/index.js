1100b6535e2904a98f004c1489419723141cf4a7
Angel-A15 <106582411+Angel-A15@users.noreply.github.com>
2022-12-21 16:05:46 -0600

    adding dependencies

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Footer() {
     return (
-        <section>
+        <section id="links">
             <p><a href="https://github.com/Angel-A15">GitHub</a></p>
             <p><a href="https://www.linkedin.com/feed/">LinkedIn</a></p>
             <p><a href="https://stackoverflow.com/users/19176451/angel-aguilar">Stackoverlow</a></p>
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -13,7 +13,7 @@ const Portfolio = (singleProject) => {
       summary: "The application is a social network for individuals who have an interest in health and fitness can share their thoughts, make friends, and communicate.",
       image: project3,
       technologies: "Node.js, JavaScript, GraphQl, React, Mern, Bootstrap.css, Express, nodemon, mongoose",
-      deployedLink: "",
+      deployedLink: "https://pacific-retreat-64211.herokuapp.com/",
       github: "https://github.com/MarkAnthony9014/fitness-project-3",
     },
     {
@@ -21,7 +21,7 @@ const Portfolio = (singleProject) => {
         summary: "The application serves by providing users resources to reach out for help wether its abuse or pertaining to mental health.",
         image: project2,
         technologies: "Node.js, Handlebars, JavaScript, BootStrap.css, Express",
-        deployedLink: "",
+        deployedLink: "https://alluda.herokuapp.com/",
         github: "https://github.com/PivotalSaint/alluda",
     },
     {
@@ -32,7 +32,7 @@ const Portfolio = (singleProject) => {
         deployedLink: "https://angel-a15.github.io/Drie-Name-Generator/",
         github: "https://github.com/Angel-A15/Drie-Name-Generator",
     }
-];
+  ];
 
   return (
     <div key={singleProject.title}>
