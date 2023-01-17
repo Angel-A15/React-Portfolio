3821f000fb437cac4b8e2debc44d482732ea0c51
Angel-A15 <106582411+Angel-A15@users.noreply.github.com>
2023-01-17 06:50:06 -0600

    restyled header for bio

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -9,27 +9,31 @@ function About() {
                 <img className="photo" src={PersonalPic} alt="latino with long black hair, glasses, and with a moustache and goatee"/>
             </div>
             
-            <div className='bio cardo col-9'>
+            <div className='bio mt-5'>
 
-                <h1 className='header'>
+                <h1 className='header text-center'>
                     About Me
                 </h1>
 
                 <h3 className='card-body text-dark mt-3'>
-                    <p> Hello, my name is Angel Aguilar and I'm a full stack web developer from central Texas.
+                    <p> 
+                        Hello, my name is Angel Aguilar and I'm a full stack web developer from central Texas.
                         I've always had an admiration for computer programming and having friends who were already
-                        in the field increased my curiosity for the field. So I have them to partially thank 
-                        for aspiring me to take the risk and initaite the action to make an interest into my career.
+                        in the field increased my curiosity for it. I thank them and my family for pushing 
+                        me to take the initaitive from turning my interests into a passionate career choice
+                        for me.
                     </p>
 
-                    <p>    I recieved a strong background, experience, and understanding for this field by attending
-                        the University of Texas at San Antonio. I graduated with certification for full stack web development
-                        by the end of the year in 2022. Languages and some concepts that I have become proficient include: 
-                        Node.Js, MongoDb, React, Express, Restful API's, Progressive Web Applications, GraphQl, and
-                        Computer Science.
+                    <p> 
+                        I gained a strong background and understanding for this field by attending the University 
+                        of Texas at San Antonio coding bootcamp. I graduated with certification for full stack web 
+                        development in the end of the year of 2022. Training from this boot camp has made me 
+                        efficient in skills such as: Node.Js, MongoDb, React, Express, Restful API's, Progressive 
+                        Web Applications, GraphQl, Computer Science, and more.
                     </p>
-                    <p>  When away from the computer, I enjoy spending my time by exercising, attending concerts, spending time with friends
-                        and family, and relaxing at home.
+                    <p> 
+                        When away from the computer; I enjoy spending time by exercising, attending concerts, 
+                        spending time with friends and family, and relaxing at home.
                     </p>
                 </h3>
 
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,8 +4,8 @@ function Footer() {
     return (
         <footer className='footer'>
             <p id="links"><a href="https://github.com/Angel-A15">GitHub</a></p>
-            <p id="links"><a href="https://www.linkedin.com/feed/">LinkedIn</a></p>
             <p id="links"><a href="https://stackoverflow.com/users/19176451/angel-aguilar">Stackoverlow</a></p>
+            <p id="links"><a href="https://www.linkedin.com/feed/">LinkedIn</a></p>
         </footer>
 
     );
