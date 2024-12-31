13d09c0df47792b2a897deba9ab5cd0dc5e1bb3c
Angel-A15 <aasupertwix15@gmail.com>
2024-12-31 08:01:03 -0600

    Updates

---
+++ precache-manifest.c5e61934a4a2b4dda366ae936bc44359.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "146b865768c0a51dad1d793d08d00e24",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "978da70b400bc4645979",
+    "url": "/React-Portfolio/static/css/2.105783c1.chunk.css"
+  },
+  {
+    "revision": "29feff5240208a3d175a",
+    "url": "/React-Portfolio/static/css/main.523a32fd.chunk.css"
+  },
+  {
+    "revision": "978da70b400bc4645979",
+    "url": "/React-Portfolio/static/js/2.8ddaa8c2.chunk.js"
+  },
+  {
+    "revision": "36640498d0bc22679f17",
+    "url": "/React-Portfolio/static/js/3.2d69d95c.chunk.js"
+  },
+  {
+    "revision": "29feff5240208a3d175a",
+    "url": "/React-Portfolio/static/js/main.610f4402.chunk.js"
+  },
+  {
+    "revision": "04996f4e1d9ee29ff3da",
+    "url": "/React-Portfolio/static/js/runtime~main.6b449fc3.js"
+  },
+  {
+    "revision": "e21bbb3a1f592e282e38a29dd865692e",
+    "url": "/React-Portfolio/static/media/Coding-Resume.e21bbb3a.pdf"
+  },
+  {
+    "revision": "797b95fa33ec564fed43861130d73272",
+    "url": "/React-Portfolio/static/media/profile.797b95fa.jpg"
+  },
+  {
+    "revision": "fa104d26dee9edd81c7c4075ee3c6ff1",
+    "url": "/React-Portfolio/static/media/project1.fa104d26.jpeg"
+  },
+  {
+    "revision": "70d08fb3092327ad54a3b1a3c67c437a",
+    "url": "/React-Portfolio/static/media/project2.70d08fb3.jpeg"
+  },
+  {
+    "revision": "9b0b0f3ba5993fda5bc7f592d4e7114c",
+    "url": "/React-Portfolio/static/media/project3.9b0b0f3b.jpeg"
+  },
+  {
+    "revision": "9cbc6f24e86b66bad244238be2f4bd0d",
+    "url": "/React-Portfolio/static/media/project4.9cbc6f24.jpeg"
+  }
+]);
